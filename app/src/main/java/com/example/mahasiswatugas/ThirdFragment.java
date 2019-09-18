61be60fb968e80039ba493e7756c8797156e2d85
savier111 <aldi.colt@yahoo.com>
2019-09-19 02:11:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mahasiswatugas/ThirdFragment.java
@@ -0,0 +1,52 @@
+package com.example.mahasiswatugas;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ThirdFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ThirdFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ThirdFragment extends Fragment {
+
+
+    public ThirdFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("ThirdFragment");
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            getFragmentManager().popBackStack();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
